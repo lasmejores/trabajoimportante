6a69ce7ba302d94482b0367e139f5f37f4e99b4c
VaniaMamaniSoliz <diosapura25ms@gmail.com>
2020-03-06 03:21:00 -0400

    fronted

---
+++ public/frontend/principal-vista.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Sesion iniciada</title>
+    
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    
+    <link rel="stylesheet" href="">
+    
+  
+    
+</head>
+<body>
+   
+   <div >
+       <img src="" >
+        <h1></h1>
+        <a href="cerrar.php">Cerrar sesion</a>
+        <a href="bigdump.php">bigdump</a>
+        <a href="/Admin/server_sql.php">admin</a>
+        <a href="/highcharts/index.php">higchart</a>
+        <a href="/Chart/chart.js.php">higchart1</a>
+    
+    
+   </div>
+   
+</body>
+</html>
